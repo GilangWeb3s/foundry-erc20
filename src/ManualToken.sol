b67d230a27d2fbefccab3fa96ce7986ff8506442
Gilang <maulana.gilang305@gmail.com>
2024-08-08 06:31:46 +0700

    inititation

---
+++ src/ManualToken.sol
@@ -0,0 +1,8 @@
+//SPDX-Lisence-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract ManualToken {
+    function name() public view returns (string memory) {
+        return "Manual Token";
+    }
+}
