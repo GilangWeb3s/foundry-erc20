ec08f4591c317c90ef519bd3858016cefd909ac4
Gilang <maulana.gilang305@gmail.com>
2024-08-08 06:52:00 +0700

    generate erc20 auto

---
+++ src/AutoToken.sol
@@ -0,0 +1,10 @@
+//SPDX-Lisence-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AutoToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("AutoToken", "AT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
