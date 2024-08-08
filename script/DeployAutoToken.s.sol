491e9268dc6e8c148f23849774d25e6e876dd14b
Gilang <maulana.gilang305@gmail.com>
2024-08-08 07:31:52 +0700

    deploy script

---
+++ script/DeployAutoToken.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {AutoToken} from "../src/AutoToken.sol";
+
+contract DeployAutoToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000 ether;
+
+    function run() external {
+        vm.startBroadcast();
+        new AutoToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+}
